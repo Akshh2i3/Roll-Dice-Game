8c2403805bb690f2d9d40169a21993d011987a3c
Akshh2i3 <akshaysethiya23@gmail.com>
2024-04-26 18:04:46 +0530

    added img

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Button from '@/components/Button'
 const page = () => {
   return (
     <div className='h-screen flex justify-center items-center px-32'>
-      <img className='rounded-3xl' src='https://media.istockphoto.com/id/481511661/vector/dice-on-white-background.jpg?s=612x612&w=0&k=20&c=5nUCyd_fUCBB3i1KGHqhHZ_g_ly3KpmNGz4UUpaAx_Q=' alt='dice img' />
+      <img className='rounded-3xl' src='/images/dices.png' alt='dice img' />
 
       <div className='w-1/2 flex flex-col items-end'>
         <p className='font-bold text-8xl uppercase mb-5'>dice game</p>
